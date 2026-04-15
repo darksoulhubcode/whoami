a1463889f76ef064a8c963735b9f548a9fed3b0b
darksoulhubcode <darksoulhubcode@gmail.com>
2026-04-15 17:54:38 +0530

    Update bloxfruits.lua

--- bloxfruits.lua
+++ bloxfruits.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/1abce7c3794d459a"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/55a22ea2f53393c702f8476c7e86ce44f23501e43c03cafb72c820cb6bf57d9b/download"))()
