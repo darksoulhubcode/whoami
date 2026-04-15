37414ce1af543d2b011054d5a0c4767beb263eaf
darksoulhubcode <darksoulhubcode@gmail.com>
2026-04-15 19:05:41 +0530

    Update arsenal.lua

--- arsenal.lua
+++ arsenal.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/0373d37ec0524037"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/ddd920de2a1ebccbe77a84683af284c2cc23fe3b8bd4c0f4c6a28f6caa980f60/download"))()
