e3cdda916a355720d8c017260ec12480480f0677
darksoulhubcode <darksoulhubcode@gmail.com>
2026-04-15 19:07:35 +0530

    Update los.lua

--- los.lua
+++ los.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/0e4398aeb1374b0a"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a9bff6858ed8d8f0bc75f69672c1789511ab38c89babfeb12c3184200db55318/download"))()
